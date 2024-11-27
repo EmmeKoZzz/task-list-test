83e624585677afb367e88760de7cb104461322d3
XMKZ <emme.ko.zzz@gmail.com>
2024-11-27 12:56:56 -0500

    create Watch hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,2 @@
 export * from './useTaskService'
+export * from './useWatch'
---
+++ src/hooks/useWatch.ts
@@ -0,0 +1,32 @@
+import {useEffect, useRef, useState} from "react";
+
+export function useWatch<T>(prop: () => T, callback: (old: T, val: T) => void, timeout: number = 100) {
+	const [isWatching, setIsWatching] = useState(true);
+	const oldValRef = useRef(prop());
+	const intervalRef = useRef<number>();
+
+	const connect = () => setIsWatching(true);
+	const disconnect = () => setIsWatching(false);
+
+	useEffect(() => {
+		if (isWatching) {
+			intervalRef.current = setInterval(() => {
+				const current = prop();
+				if (current !== oldValRef.current) {
+					callback(oldValRef.current, current)
+					oldValRef.current = current;
+					clearInterval(intervalRef.current);
+				}
+			}, timeout);
+			return;
+		}
+		
+		clearInterval(intervalRef.current);
+		intervalRef.current = undefined;
+		
+	}, [callback, prop, timeout, isWatching]);
+
+	return {
+		isWatching, connect, disconnect
+	}
+}
